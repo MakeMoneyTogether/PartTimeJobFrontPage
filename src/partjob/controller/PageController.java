51a025d0d2976e86b810e46619680cc6e680323b
InnerAc <anjicun@live.com>
2017-03-30 10:35:11 +0800

    创建了工程，通过了测试
    创建了工程，通过了测试

---
+++ src/partjob/controller/PageController.java
@@ -0,0 +1,22 @@
+package partjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping("restful")
+	@ResponseBody
+	public String restful(){
+		String res = "success";
+		return res;
+	}
+}
